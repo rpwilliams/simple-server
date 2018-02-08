d143209d4b1778a7ec50deb1129b5a4e24cedbcd
Ryan Williams <rpwilliams96@gmail.com>
2018-02-07 19:33:03 -0600

    Fixed broken stuff

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const PORT = 8080;
 
-function serveIndex(path, req, res) {
+function serveIndex(path, res) {
 	fs.readdir(path, function(err, files) {
 		if(err) {
 			console.log(err);
@@ -15,10 +15,6 @@ function serveIndex(path, req, res) {
 		var html = "<p>Index of " + path + "</p>";
         html += "<ul>";
         html += files.map(function(item){
-        	if(req.url !== '/') {
-        		item = req.url + '/' + item;
-        		console.log(item);
-        	}
             return "<li><a href='" + item + "'>" + item + "</a></li>";
         }).join("");
         html += "</ul>";
@@ -61,30 +57,42 @@ function handleRequest(req, res) {
     // Map request urls to files
     switch(req.url) {
         case '/':
-            serveIndex('public', req, res);
+            serveIndex('public', res);
             break;
         default:
         	var path = 'public' + req.url;
-        	if(isDirectory(path)) {
-        		serveIndex(path, req, res);
+        	if(isDirectory(path, res)) {
+        		console.log(path + " is a directory! Now serving the index...");
+        		serveIndex(path, res);
         	}
-        	else if(isFile(path)) {
+        	else if(isFile(path, res)) {
+        		console.log(path + " is a file! Now serving the file...");
         		serveFile(path, res);
         	}
         	break;
     }
 }
 
-function isDirectory(path) {
-	console.log("Checking if " + path + " is a file");
-
-	return fs.lstatSync(path).isDirectory();
+function isDirectory(path, res) {
+	console.log("Checking if " + path + " is a directory...");
+	if(fs.existsSync(path)) {
+		return fs.lstatSync(path).isDirectory();
+	}
+	else {
+		console.log("Directory could not be found.");
+		return;
+	}
 }
 
-function isFile(path) {
-	console.log("Checking if " + path + " is a file");
-
-	return fs.lstatSync(path).isFile();
+function isFile(path, res) {
+	console.log("Checking if " + path + " is a file...");
+	if(fs.existsSync(path)) {
+		return fs.lstatSync(path).isFile();
+	}
+	else {
+		console.log("File could not be found.");
+		return;
+	}
 }
 
 // Create the web server
